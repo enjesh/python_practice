099da7eba825c4492f6d255a85edd63dcc203dbc
enjesh <enjesh@gmail.com>
2022-11-29 23:31:54 +0300

    Learning for + if statement and range

---
+++ range.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    print(i)
+
+# list(range(5, 10))
+# list(range(0, 10, 3))
+# list(range(-10, -100, -30))
+# range (10)
+# Sum is iterable
+# sum(range(4))  # 0 + 1 + 2 + 3
+
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])

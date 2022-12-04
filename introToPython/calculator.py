d281d05f56bce65e3fb8e83b0f634f3e1d029bd2
enjesh <enjesh@gmail.com>
2022-12-04 16:11:59 +0300

    Edit to the README and more calculation examples

--- introToPython/calculator.py
+++ introToPython/calculator.py
@@ -20,3 +20,38 @@ print (x)
 
 y = width **2
 print (y)
+
+# You can conver the float to an integer and vise versa
+sum = 3.4 + 2.7
+print(sum)
+print(type(sum))
+sum = int(sum)
+print(sum)
+print(type(sum))
+
+# PEMDAS
+cal = 5 * (3**2 + 5) - 8/2
+print(cal)
+
+cakes = 12
+pies = 4
+desserts = cakes + pies
+print (desserts)
+
+# (2 × 3) + 72
+Python = (2 * 3) + 7**2
+print(Python)
+
+# 72 / 8
+Python = 72 / 8
+print(Python)
+
+# 33 / 2 + 32
+Python = 3**3 / 2 + 3**2
+print(Python)
+
+# (5 + 10) + (9 × 5) − 12
+Python = (5 + 10) + (9 * 5) - 12
+# (15) + (45) - 12
+# 60 - 12
+print(Python)

d4f2467a03e27ded16213e233d3f15897c075f06
enjesh <enjesh@gmail.com>
2022-12-07 01:04:08 +0300

    More on string formatting

--- introToPython/f-string.py
+++ introToPython/f-string.py
@@ -1,4 +1,4 @@
-#  Formatted string literals, 
+#  Formatted string literals (f strings)
 year = 2016
 event = 'Referendum'
 print (f'Results of the {year} {event}')
@@ -9,3 +9,104 @@ no_votes = 43_132_495
 percentage = yes_votes / (yes_votes + no_votes)
 print ('{:-9} YES votes  {:2.2%}'.format(yes_votes, percentage))
 
+# Strings have two distinct representations str() and repr()
+s = 'Hello, world.'
+print(str(s))
+
+print(repr(s))
+
+print(str(1/7))
+
+x = 10 * 3.25
+y = 200 * 200
+s = 'The value of x is ' + repr(x) + ', and y is ' + repr(y) + '...'
+print(s)
+
+# The repr() of a string adds string quotes and backslashes:
+hello = 'hello, world\n'
+hellos = repr(hello)
+print(hellos)
+
+# The argument to repr() may be any Python object:
+print(repr((x, y, ('spam', 'eggs'))))
+
+
+import math
+print(f'The value of pi is approximately {math.pi:.3f}.')
+
+
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 7678}
+for name, phone in table.items():
+    print(f'{name:10} ==> {phone:10d}')
+
+
+#  '!a' applies ascii(), '!s' applies str(), and '!r' applies repr()
+animals = 'eels'
+print(f'My hovercraft is full of {animals}.')
+
+print(f'My hovercraft is full of {animals!r}.')
+
+print(f'My hovercraft is full of {animals!a}.')
+
+print(f'My hovercraft is full of {animals!s}.')
+
+
+# = specifier - expand an expression to the text of the expression = then the representation of the evaluated expression
+bugs = 'roaches'
+count = 13
+area = 'living room'
+print(f'Debugging {bugs=} {count=} {area=}')
+
+
+print('We are the {} who say "{}!"'.format('knights', 'Ni'))
+
+# A number in the brackets used to refer to the position of the object passed into the str.format() method.
+print('{0} and {1}'.format('spam', 'eggs'))
+
+print('{1} and {0}'.format('spam', 'eggs'))
+
+
+# keyword arguments
+print('This {food} is {adjective}.'.format(
+      food='spam', adjective='absolutely horrible'))
+
+
+# Positional and keyword arguments combined
+print('The story of {0}, {1}, and {other}.'.format('Bill', 'Manfred',
+                                                   other='Georg'))
+
+
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 8637678}
+print('Jack: {0[Jack]:d}; Sjoerd: {0[Sjoerd]:d}; '
+      'Dcab: {0[Dcab]:d}'.format(table))
+
+
+table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 8637678}
+print('Jack: {Jack:d}; Sjoerd: {Sjoerd:d}; Dcab: {Dcab:d}'.format(**table))
+
+
+# integers and their squares and cubes
+for x in range(1, 11):
+    print('{0:2d} {1:3d} {2:4d}'.format(x, x*x, x*x*x))
+
+
+# The str.rjust() method right-justifies a string by padding it with spaces on the left.
+#  str.zfill(), which pads a numeric string on the left with zeros.
+for x in range(1, 11):
+    print(repr(x).rjust(2), repr(x*x).rjust(3), end=' ')
+    # Note use of 'end' on previous line
+    print(repr(x*x*x).rjust(4))
+
+
+
+
+print('12'.zfill(5))
+
+print('-3.14'.zfill(7))
+
+print('3.14159265359'.zfill(5))
+
+
+# Old string formatting
+import math
+print('The value of pi is approximately %5.3f.' % math.pi)

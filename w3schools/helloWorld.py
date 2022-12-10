603f2fbf40d5be72dc50b1677e1f0255dd486885
enjesh <enjesh@gmail.com>
2022-12-10 23:23:21 +0300

    Learning python from w3schools

---
+++ w3schools/helloWorld.py
@@ -0,0 +1,5 @@
+print ( "Hello World!" )
+
+print("Hello from Nairobi, Kenya") 
+
+print("Python is hella fun to learn!!!!!")
---
+++ w3schools/syntax.py
@@ -0,0 +1,14 @@
+# Python uses indentation to indicate a block of code
+# Indentation is not only for readability
+if 5 > 2:
+    print ("Five is greater than two!")
+
+
+# variables are created when you assign a value to it
+x = 5
+y = 2
+z = "Hello, World!"
+
+print(x,y,z)
+
+

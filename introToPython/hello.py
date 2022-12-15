251b4040e59fcf71691aae78beede918ffd4dcdb
enjesh <enjesh@gmail.com>
2022-12-16 00:21:10 +0300

    Created this file with shutil module

---
+++ introToPython/hello.py
@@ -0,0 +1,60 @@
+# shutil.copyfile('calculator.py', 'hello.py')
+# I used the shutil module to create this file
+
+2 + 2 # Addition
+50 - 5 # Subtraction
+50 - 5 * 6 # Multiplication
+(50 - 5 * 6) / 4 # Division
+
+8 / 5 # Division always returns a floationg point number
+17 / 3
+
+17 // 3 # floor division discards the fractional part
+17 % 3 # the % operator returns the remainder of the division
+5 * 3 + 2 # floored quotient * divisor + remainder
+
+5 ** 2 # 5 squared
+2 ** 7 # 2 to the power of 7
+
+width = 20
+height = 5 * 9
+x = width * height
+print (x)
+
+y = width **2
+print (y)
+
+# You can conver the float to an integer and vise versa
+sum = 3.4 + 2.7
+print(sum)
+print(type(sum))
+sum = int(sum)
+print(sum)
+print(type(sum))
+
+# PEMDAS
+cal = 5 * (3**2 + 5) - 8/2
+print(cal)
+
+cakes = 12
+pies = 4
+desserts = cakes + pies
+print (desserts)
+
+# (2 × 3) + 72
+Python = (2 * 3) + 7**2
+print(Python)
+
+# 72 / 8
+Python = 72 / 8
+print(Python)
+
+# 33 / 2 + 32
+Python = 3**3 / 2 + 3**2
+print(Python)
+
+# (5 + 10) + (9 × 5) − 12
+Python = (5 + 10) + (9 * 5) - 12
+# (15) + (45) - 12
+# 60 - 12
+print(Python)
